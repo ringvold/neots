103624dce483dca4485519fab1937580f85f7197
Harald Ringvold <harald.ringvold@gmail.com>
2022-12-25 23:34:07 +0100

    Naive implementation without error handling

--- src/main.rs
+++ src/main.rs
@@ -4,8 +4,21 @@ use clap::{Parser, Subcommand};
 use std::io::{Write};
 use chacha20poly1305::{
     aead::{Aead, AeadCore, KeyInit, OsRng},
-    ChaCha20Poly1305, Nonce, Error
+    ChaCha20Poly1305, Error
 };
+use serde::Deserialize;
+use base64::{encode};
+const URL_SAFE_ENGINE: base64::engine::fast_portable::FastPortable =
+    base64::engine::fast_portable::FastPortable::from(
+        &base64::alphabet::URL_SAFE,
+        base64::engine::fast_portable::PAD);
+
+#[derive(Deserialize, Debug)]
+struct CreateResponse {
+    #[serde(rename = "expiresAt")]
+    expires_at: u32,
+    id: String,
+}
 
 #[derive(Parser, Debug)]
 #[command(author, version, about, long_about = Some("Encrypts a secret and makes it available for sharing via one-time URL.
@@ -44,20 +57,47 @@ fn main() {
 
 }
 
-
 fn new(_expiration: Option<String>) {
     println!("");
     println!("Enter your secret:");
+
     std::io::stdout().flush().unwrap();
     let secret = read_password().unwrap();
+
     println!("Secret: {}", secret);
     println!("Encrypting..");
-    println!("Sike! Must implement first");
+
+    match encrypt(secret) {
+        (Ok(ciphertext), nonce, key) => {
+            // TODO: Might be a better way to concat
+            let concat: Vec<u8> = nonce.into_iter().chain(ciphertext.into_iter()).collect();
+            let encoded = encode(&concat);
+            let client = reqwest::blocking::Client::new();
+            let res: CreateResponse = client
+                .post("http://localhost:4000/api")
+                .json(&serde_json::json!({
+                    "encryptedBytes": encoded,
+                    "expiresIn": 7200,
+                    "cipher": "chachapoly"
+                }))
+                .send()
+                .unwrap()
+                .json()
+                .unwrap();
+            println!("URL: {:?}", create_url(key, res.id))
+        },
+        (Err(err),_,_) => panic!("{:?}", err),
+    };
 }
 
-fn encrypt(secret: String) -> Result<Vec<u8>, Error> {
+fn encrypt(secret: String) -> (Result<Vec<u8>, Error>, Vec<u8>,Vec<u8>) {
     let key = ChaCha20Poly1305::generate_key(&mut OsRng);
     let cipher = ChaCha20Poly1305::new(&key);
     let nonce = ChaCha20Poly1305::generate_nonce(&mut OsRng); // 96-bits; unique per message
-    return cipher.encrypt(&nonce, b"plaintext message".as_ref());
+    (cipher.encrypt(&nonce, secret.as_ref()), nonce.to_vec(), key.to_vec())
+}
+
+fn create_url(key: Vec<u8>, id: String) -> String {
+    let encoded_key = base64::encode_engine(key, &URL_SAFE_ENGINE);
+    format!("http://localhost:4000/view/{id}?ref=neots#{key}", id = id, key = encoded_key)
 }
